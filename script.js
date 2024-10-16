c85c0568ae8efbf4a4659967936dc5e065ecbaa0
Meera Godden <meera.s.gd@gmail.com>
2024-10-15 19:19:57 -0700

    Change alignment and button styling

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-
 function drawGrid(size) {
-	const container = document.querySelector('.container');
-	container.textContent = '';
+  const container = document.querySelector(".container");
+  container.textContent = "";
 
-	for (let i = 0; i < size * size; i++) {
-		const square = document.createElement('div');
-		square.classList.add('square');
-		square.style.minWidth = 960 / size + 'px';
-		square.style.minHeight = 960 / size + 'px';
-		square.addEventListener('mouseenter', (e) => {
-			e.target.classList.add('highlighted');
-		});
-		container.appendChild(square);
-	}
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.minWidth = 500 / size + "px";
+    square.style.minHeight = 500 / size + "px";
+    square.addEventListener("mouseenter", (e) => {
+      e.target.classList.add("highlighted");
+    });
+    container.appendChild(square);
+  }
 }
 
-const sizeBtn = document.querySelector('#sizer');
-sizeBtn.addEventListener('click', () => {
-	let size = +prompt('How many squares wide should the grid be (max 100)?');
-	size = Math.min(100, size);
-	drawGrid(size);
-})
+const sizeBtn = document.querySelector("#sizer");
+sizeBtn.addEventListener("click", () => {
+  let size = +prompt("How many squares wide should the grid be (max 100)?");
+  size = Math.min(100, size);
+  drawGrid(size);
+});
 
 drawGrid(16);
-
